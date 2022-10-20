649d481269d1c1a9482164f28b9815bfbc5848fb
Jacob Estep <jacobwestep@gmail.com>
2022-10-19 21:03:16 -0700

    new mac, updated maps API and added to gitignore

--- honeycomb_test/ios/Runner/AppDelegate.swift
+++ honeycomb_test/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyC1hEwGPXOck5HeY0ziBFtNGZ7GJGa5HAs")
+    GMSServices.provideAPIKey("AIzaSyBwBhS4U9BQNQ2FJzJwGKlkeoBjIukb7_Q")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
