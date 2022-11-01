bd23c63c86d36741e6db19906be72da8d6c92f70
shlo <shlo.sam@gmail.com>
2022-11-01 12:53:57 -0700

    location and geocode

---
+++ geo_test/lib/geo_helper.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class GeoHelper {
+  final apiKey = '';
+
+  GeoHelper();
+
+  Future<String> parseZipCode(String zipCode) async {
+    try {
+      final url = Uri.parse(
+          'https://maps.googleapis.com/maps/api/geocode/json?address=$zipCode&key=$apiKey');
+      final response = await http.get(url);
+      final data = jsonDecode(response.body);
+      final formattedAddress = data['results'].first['formatted_address'];
+
+      return formattedAddress;
+    } catch (e) {
+      // print(e);
+      return '';
+    }
+  }
+
+  Future<String> parseLatLong(double lat, double long) async {
+    try {
+      final url = Uri.parse(
+          'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$long&key=$apiKey');
+      final response = await http.get(url);
+      final data = jsonDecode(response.body);
+      final formattedAddress = data['results'].first['formatted_address'];
+
+      return formattedAddress;
+    } catch (e) {
+      // print(e);
+      return '';
+    }
+  }
+
+  Future<Map> parseAddress(String address, String zipCode) async {
+    try {
+      final suffix = parseZipCode(zipCode);
+
+      final fullAddress = '$address, $suffix';
+
+      final url = Uri.parse(
+          'https://maps.googleapis.com/maps/api/geocode/json?address=$fullAddress&key=$apiKey');
+      final response = await http.get(url);
+      final data = jsonDecode(response.body);
+      return data['results'].first['geometry']['location'];
+    } catch (e) {
+      // print(e);
+      return {};
+    }
+  }
+}
---
+++ geo_test/lib/main.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+import './geo_helper.dart';
+import 'package:geolocator/geolocator.dart';
+
+void main() async {
+  print('test');
+
+  final geoHelper = GeoHelper();
+
+  // final suffix = await geoHelper.parseZipCode('98052');
+
+  final pos = await geoHelper.parseAddress('12280 NE District Wy', '98005');
+  if (pos.isNotEmpty) {
+    final fullAddress = await geoHelper.parseLatLong(pos['lat'], pos['lng']);
+    print(fullAddress);
+  }
+
+  // final fullAddress = await geoHelper.parseLatLong(pos['lat'], pos['lng']);
+
+  // print(fullAddress);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  String _pos = '';
+
+  // @override
+  // void initState() {
+  //   // TODO: implement initState
+  //   super.initState();
+  //   final pos = await _determinePosition();
+  //   print('$pos.latitude, $pos.longitude');
+  // }
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
+  void _incrementCounter() async {
+    final pos = await _determinePosition();
+    final fullAddress =
+        await GeoHelper().parseLatLong(pos.latitude, pos.longitude);
+    print(fullAddress);
+
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+      // final lat = pos.latitude;
+      // final lng = pos.longitude;
+
+      _pos = fullAddress;
+      // print('$pos.latitude, $pos.longitude');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              _pos,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
