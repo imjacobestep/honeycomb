c8417abb3e9905b4e2f9adc7532d318cfc30c7d6
Jacob Estep <jacobwestep@gmail.com>
2022-11-01 15:41:36 -0700

    added text handling for map search

--- honeycomb_test/lib/pages/bottomNav/map.dart
+++ honeycomb_test/lib/pages/bottomNav/map.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:honeycomb_test/geo_helper.dart';
 import 'package:honeycomb_test/model/resource.dart';
 import 'package:honeycomb_test/pages/resource_details.dart';
 import 'package:honeycomb_test/proxy.dart';
@@ -15,9 +16,16 @@ class MapPage extends StatefulWidget {
   MapPageState createState() => MapPageState();
   //ResourceList mainList;
   Proxy proxyModel = Proxy();
+  GeoHelper geo = GeoHelper();
   Future<Iterable>? resourceList;
-  Location location = new Location();
-  Future<LocationData>? loc;
+  Location location = Location();
+  Future<LocationData>? currentLocation;
+  Future<Map<dynamic, dynamic>>? typedLocation;
+  bool? useCurrentLocation;
+  String typedAddress = "12280 NE District Wy";
+  String typedZipCode = "98005";
+  Completer<GoogleMapController>? _controller;
+  bool showReturnLocation = false;
 
   MapPage();
 }
@@ -25,9 +33,16 @@ class MapPage extends StatefulWidget {
 class MapPageState extends State<MapPage> {
   @override
   void initState() {
+    widget._controller = Completer();
     //widget.unfilteredList = await widget.proxyModel.list('resources');
     widget.resourceList = widget.proxyModel.list("resources");
-    widget.loc = widget.location.getLocation();
+    widget.currentLocation = widget.location.getLocation();
+    /* widget.typedLocation =
+        widget.geo.parseAddress(widget.typedAddress, widget.typedZipCode); */
+    widget.typedLocation =
+        widget.geo.parseAddress("12280 NE District Wy", "98005");
+    widget.useCurrentLocation = true;
+    widget.useCurrentLocation = false;
     super.initState();
   }
 
@@ -66,8 +81,6 @@ class MapPageState extends State<MapPage> {
             BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure)));
   }
 
-  final Completer<GoogleMapController> _controller = Completer();
-
   String getCategories(Resource resource) {
     //String ret = "";
     if (resource.categories != null) {
@@ -77,153 +90,187 @@ class MapPageState extends State<MapPage> {
     }
   }
 
+  void buildList(Iterable resourceList) {
+    for (Resource resource in resourceList) {
+      addMarker(resource);
+    }
+  }
+
 // UI WIDGETS
-  Widget getMap() {
+  Widget noResources() {
+    return const Padding(
+      padding: EdgeInsets.all(16),
+      child: Text("No Results"),
+    );
+  }
+
+  Widget getMap(CameraPosition cam) {
+    return GoogleMap(
+      myLocationEnabled: true,
+      myLocationButtonEnabled: widget.showReturnLocation,
+      mapType: MapType.normal,
+      markers: markers,
+      initialCameraPosition: cam,
+      onMapCreated: (GoogleMapController controller) {
+        widget._controller!.complete(controller);
+      },
+    );
+  }
+
+  Widget buildCurrentLocation() {
+    return FutureBuilder(
+        future: widget.currentLocation,
+        builder: (BuildContext context, AsyncSnapshot loc) {
+          CameraPosition cam;
+          LatLng location;
+          if (loc.hasData &&
+              (loc.data!.latitude != null && loc.data!.longitude != null)) {
+            location = LatLng(loc.data!.latitude!, loc.data!.longitude!);
+            cam = CameraPosition(
+              target: location,
+              zoom: 17.4746,
+            );
+            //currentMarker(location);
+            return getMap(cam);
+          } else {
+            return LoadingIndicator(
+              size: 50,
+              borderWidth: 5,
+              color: Theme.of(context).colorScheme.primary,
+            );
+          }
+        });
+  }
+
+  Widget buildClientLocation() {
+    return FutureBuilder(
+        future: widget.typedLocation,
+        builder: (BuildContext context, AsyncSnapshot loc) {
+          CameraPosition cam;
+          LatLng location;
+          if (loc.hasData) {
+            //print(loc.data);
+            if (loc.data!['lat'] != null && loc.data!['lng'] != null) {
+              location = LatLng(loc.data!['lat']!, loc.data!['lng']!);
+              cam = CameraPosition(
+                target: location,
+                zoom: 17.4746,
+              );
+              //currentMarker(location);
+              return getMap(cam);
+            } else {
+              return const Center(
+                  child: Text(
+                "invalid address",
+              ));
+            }
+          } else {
+            return LoadingIndicator(
+              size: 50,
+              borderWidth: 5,
+              color: Theme.of(context).colorScheme.primary,
+            );
+          }
+        });
+  }
+
+  Widget buildMap() {
     return FutureBuilder(
       future: widget.resourceList,
-      builder: (BuildContext context, AsyncSnapshot<Iterable> snapshot) {
-        List<Widget> children = [];
-        if (snapshot.hasData && snapshot.data != null) {
-          Iterable testList = snapshot.data!;
-          for (Resource resource in testList) {
-            addMarker(resource);
-          }
+      builder: (BuildContext context, AsyncSnapshot<Iterable> resourceResults) {
+        if (resourceResults.hasData && resourceResults.data != null) {
+          buildList(resourceResults.data!);
         } else {
-          return const Padding(
-            padding: EdgeInsets.all(16),
-            child: Text("No Results"),
-          );
+          return noResources();
         }
-        return FutureBuilder(
-            future: widget.loc,
-            builder:
-                (BuildContext context, AsyncSnapshot<LocationData> snapshot2) {
-              CameraPosition cam;
-              LatLng location;
-              if (snapshot2.hasData &&
-                  (snapshot2.data!.latitude != null &&
-                      snapshot2.data!.longitude != null)) {
-                location = LatLng(
-                    snapshot2.data!.latitude!, snapshot2.data!.longitude!);
-                cam = CameraPosition(
-                  target: location,
-                  zoom: 17.4746,
-                );
-                currentMarker(location);
-                return GoogleMap(
-                  myLocationEnabled: true,
-                  myLocationButtonEnabled: true,
-                  mapType: MapType.normal,
-                  markers: markers,
-                  initialCameraPosition: cam,
-                  //onCameraMove: onCameraMove,
-                  onMapCreated: (GoogleMapController controller) {
-                    _controller.complete(controller);
-                  },
-                );
-              } else {
-                return const LoadingIndicator(size: 50, borderWidth: 5);
-              }
-            });
+        if (widget.useCurrentLocation != null && widget.useCurrentLocation!) {
+          return buildCurrentLocation();
+        } else {
+          return buildClientLocation();
+        }
       },
+    );
+  }
+
+  Widget searchBar() {
+    return TextField(
+      maxLines: 1,
+      textAlignVertical: TextAlignVertical.center,
+      onSubmitted: (text) {
+        setState(() {
+          final split = text.split(',');
+          widget.typedAddress = split[0];
+          widget.typedZipCode = split[split.length - 1];
+          widget.useCurrentLocation = false;
+          widget._controller = Completer();
+          widget.showReturnLocation = false;
+        });
+      },
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(0),
+        isDense: true,
+        constraints: const BoxConstraints(maxHeight: 50),
+        filled: true,
+        hintText: "Search address...",
+        prefixIcon: const Icon(Icons.search_outlined),
+        fillColor: Theme.of(context).canvasColor,
+        border: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(30)),
+        ),
+      ),
+    );
+  }
+
+  Widget headerButton() {
+    return ElevatedButton(
+        onPressed: () {
+          setState(() {
+            widget.useCurrentLocation = true;
+            widget._controller = Completer();
+            widget.showReturnLocation = true;
+          });
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            const SizedBox(height: 50, child: Center(child: Text("Me"))),
+            //getSpacer(2),
+            const Icon(Icons.my_location)
+          ],
+        ));
+  }
+
+  PreferredSizeWidget topHeader() {
+    return AppBar(
+      toolbarHeight: 80,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(40))),
+      title: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(flex: 20, child: searchBar()),
+          Expanded(
+            flex: 1,
+            child: getSpacer(4),
+          ),
+          Expanded(flex: 8, child: headerButton())
+        ],
+      ),
+      backgroundColor: const Color(0xFF2B2A2A),
+      foregroundColor: Colors.white,
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 80,
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(bottom: Radius.circular(40))),
-        title: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              flex: 20,
-              child: TextField(
-                decoration: InputDecoration(
-                  constraints: const BoxConstraints(maxHeight: 50),
-                  filled: true,
-                  hintText: "Search...",
-                  prefixIcon: const Icon(Icons.search_outlined),
-                  fillColor: Theme.of(context).canvasColor,
-                  border: const OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(30)),
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: getSpacer(4),
-            ),
-            //getSpacer(8),
-            Expanded(
-              flex: 8,
-              child: ElevatedButton(
-                  onPressed: () {},
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.max,
-                    // ignore: prefer_const_literals_to_create_immutables
-                    children: [
-                      const SizedBox(
-                          height: 50, child: Center(child: Text("Add"))),
-                      //getSpacer(2),
-                      const Icon(Icons.add_circle_outline)
-                    ],
-                  )),
-            )
-          ],
-        ),
-        backgroundColor: const Color(0xFF2B2A2A),
-        foregroundColor: Colors.white,
-      ),
-      /*appBar: AppBar(
-        toolbarHeight: 80,
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))),
-        title: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              flex: 2,
-              child: ElevatedButton(
-                  onPressed: () {},
-                  child: Row(
-                    children: [
-                      const Icon(Icons.location_on_outlined),
-                      getSpacer(4),
-                      const Text("my location")
-                    ],
-                  )),
-            ),
-            getSpacer(8),
-            Expanded(
-              flex: 1,
-              child: ElevatedButton(
-                  onPressed: () {},
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      const Text("Filters"),
-                      getSpacer(4),
-                      const Icon(Icons.filter_list)
-                    ],
-                  )),
-            )
-          ],
-        ),
-        backgroundColor: const Color(0xFF2B2A2A),
-        foregroundColor: Colors.white,
-      ),*/
+      appBar: topHeader(),
       body: Stack(
-        children: [getMap()],
+        children: [buildMap()],
       ),
       bottomNavigationBar: customNav(context, 1),
     );
